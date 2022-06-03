c9b3dc9e50a4e891eaa7ac4b7c3c2c65e28effeb
Andrey Vakarchuk <dronn1k.dev@gmail.com>
2022-06-03 12:47:00 +0300

    removed "Share" button.

--- lib/src/presentation/bar_tools/bottom_tools.dart
+++ lib/src/presentation/bar_tools/bottom_tools.dart
@@ -5,10 +5,7 @@ import 'package:stories_editor/src/domain/providers/notifiers/control_provider.d
 import 'package:stories_editor/src/domain/providers/notifiers/draggable_widget_notifier.dart';
 import 'package:stories_editor/src/domain/providers/notifiers/painting_notifier.dart';
 import 'package:stories_editor/src/domain/providers/notifiers/scroll_notifier.dart';
-import 'package:stories_editor/src/domain/sevices/save_as_image.dart';
 import 'package:stories_editor/src/presentation/utils/constants/item_type.dart';
-import 'package:stories_editor/src/presentation/utils/constants/text_animation_type.dart';
-import 'package:stories_editor/src/presentation/widgets/animated_onTap_button.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 class BottomTools extends StatelessWidget {
@@ -63,6 +60,7 @@ class BottomTools extends StatelessWidget {
                                       await AssetPicker.pickAssets(context,
                                           pickerConfig: const AssetPickerConfig(
                                             maxAssets: 1,
+                                            requestType: RequestType.image,
                                           ));
                                   if (files != null && files.isNotEmpty) {
                                     controlNotifier.mediaPath =
@@ -139,79 +137,7 @@ class BottomTools extends StatelessWidget {
               /// save final image to gallery
               Container(
                 width: _size.width / 3,
-                alignment: Alignment.centerRight,
                 padding: const EdgeInsets.only(right: 15),
-                child: Transform.scale(
-                  scale: 0.9,
-                  child: StatefulBuilder(
-                    builder: (_, setState) {
-                      return AnimatedOnTapButton(
-                          onTap: () async {
-                            String pngUri;
-                            if (paintingNotifier.lines.isNotEmpty ||
-                                itemNotifier.draggableWidget.isNotEmpty) {
-                              for (var element
-                                  in itemNotifier.draggableWidget) {
-                                if (element.animationType !=
-                                    TextAnimationType.none) {
-                                  setState(() {
-                                    _createVideo = true;
-                                  });
-                                }
-                              }
-                              if (_createVideo) {
-                                debugPrint('creating video');
-                                await renderWidget();
-                              } else {
-                                debugPrint('creating image');
-                                await takePicture(
-                                        contentKey: contentKey,
-                                        context: context,
-                                        saveToGallery: false)
-                                    .then((bytes) {
-                                  if (bytes != null) {
-                                    pngUri = bytes;
-                                    onDone(pngUri);
-                                  } else {}
-                                });
-                              }
-                            }
-                            setState(() {
-                              _createVideo = false;
-                            });
-                          },
-                          child: onDoneButtonStyle ??
-                              Container(
-                                padding: const EdgeInsets.only(
-                                    left: 12, right: 5, top: 4, bottom: 4),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15),
-                                    border: Border.all(
-                                        color: Colors.white, width: 1.5)),
-                                child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: const [
-                                      Text(
-                                        'Share',
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            letterSpacing: 1.5,
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w400),
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsets.only(left: 5),
-                                        child: Icon(
-                                          Icons.arrow_forward_ios,
-                                          color: Colors.white,
-                                          size: 15,
-                                        ),
-                                      ),
-                                    ]),
-                              ));
-                    },
-                  ),
-                ),
               ),
             ],
           ),
